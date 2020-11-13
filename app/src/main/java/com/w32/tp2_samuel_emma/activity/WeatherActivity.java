877fb263dbdfed3ec2ea419d553bcd2548b5253a
EmFa-1934931 <70659646+EmFa-1934931@users.noreply.github.com>
2020-11-13 15:52:58 -0500

    fin parcels, début affichage

--- app/src/main/java/com/w32/tp2_samuel_emma/activity/WeatherActivity.java
+++ app/src/main/java/com/w32/tp2_samuel_emma/activity/WeatherActivity.java
@@ -5,24 +5,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.w32.tp2_samuel_emma.R;
 import com.w32.tp2_samuel_emma.sensor.SensorData;
+import com.w32.tp2_samuel_emma.sensor.SensorValue;
 
 public class WeatherActivity extends AppCompatActivity {
-    //TODO: Sérialisation des parcelable (avec les données des capteurs)
     //TODO: Affichage du nombre de données
 
     SensorData values;
+    SensorValue[] valuesArray;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+
+        Intent intent = getIntent();
+        if (intent.hasExtra("SENSOR_PARCEL")) {
+            values = intent.getParcelableExtra("SENSOR_PARCEL");
+        }
+
+        valuesArray = values.getValues();
+        showDataCount(countValues());
     }
 
     public void onGoBack(View view) {
         onBackPressed();
     }
 
+    private int countValues(){
+        int count = 0;
+        while (count < valuesArray.length){
+            count++;
+        }
+        return count;
+    }
+
+    private void showDataCount(int count){
+        String output = count + " données ont été enregistrées.";
+        textView.setText(output);
+    }
 }
