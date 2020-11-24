522ee46db5c5811eafeefb9279625a675ff91883
EmFa-1934931 <70659646+EmFa-1934931@users.noreply.github.com>
2020-11-24 09:31:16 -0500

    ajout activité température

---
+++ app/src/main/java/com/w32/tp2_samuel_emma/activity/TemperatureActivity.java
@@ -0,0 +1,16 @@
+package com.w32.tp2_samuel_emma.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.w32.tp2_samuel_emma.R;
+
+public class TemperatureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temperature);
+    }
+}
