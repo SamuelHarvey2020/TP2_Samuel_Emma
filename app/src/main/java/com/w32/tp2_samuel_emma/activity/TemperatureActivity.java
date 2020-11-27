2d76a2aebc831ce4a9a0d825409ab8864c5b7890
EmFa-1934931 <70659646+EmFa-1934931@users.noreply.github.com>
2020-11-27 18:50:08 -0500

    suite activité température

--- app/src/main/java/com/w32/tp2_samuel_emma/activity/TemperatureActivity.java
+++ app/src/main/java/com/w32/tp2_samuel_emma/activity/TemperatureActivity.java
@@ -2,15 +2,98 @@ package com.w32.tp2_samuel_emma.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 import com.w32.tp2_samuel_emma.R;
+import com.w32.tp2_samuel_emma.model.ModelControllerTemperature;
+import com.w32.tp2_samuel_emma.sensor.SensorData;
+import com.w32.tp2_samuel_emma.sensor.SensorValue;
 
 public class TemperatureActivity extends AppCompatActivity {
+    SensorData values;
+    GraphView graph;
+    ModelControllerTemperature controller;
+    ImageButton upMaxBtn;
+    ImageButton downMaxBtn;
+    ImageButton upMinBtn;
+    ImageButton downMinBtn;
+    private TextView maxTxt;
+    private TextView minTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //Cache le titre de l'application
+        getSupportActionBar().hide(); //cache l'espace du titre
         setContentView(R.layout.activity_temperature);
+
+        maxTxt = findViewById(R.id.maxTempNb);
+        minTxt = findViewById(R.id.minTempNb);
+        graph = (GraphView) findViewById(R.id.graph);
+        upMaxBtn = findViewById(R.id.upMaxTemp);
+        downMaxBtn = findViewById(R.id.downMaxTemp);
+        upMinBtn = findViewById(R.id.upMinTemp);
+        downMinBtn = findViewById(R.id.downMinTemp);
+
+        if (savedInstanceState != null) {
+            values = savedInstanceState.getParcelable("SI_PARCEL_SENSOR");
+        }
+
+        //Recherche de l'intent explicite et implicite
+        Intent intent = getIntent();
+        if (intent.hasExtra("SENSOR_PARCEL")) {
+            values = intent.getParcelableExtra("SENSOR_PARCEL");
+        }
+
+        controller = new ModelControllerTemperature(values);
+        updateTextCount();
+        addGraphData();
+    }
+
+    private void addGraphData(){
+        DataPoint[] data = new DataPoint[values.getValues().length];
+        int i = 0;
+        for(SensorValue sensorValues: values.getValues()){
+            data[i] = new DataPoint(sensorValues.getTimeStamp(), sensorValues.getValue());
+            i++;
+        }
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(data);
+        graph.addSeries(series);
+        graph.getLegendRenderer().setVisible(true);
+    }
+
+    private void updateTextCount(){
+        maxTxt.setText(Double.toString(controller.getHighLimit()));
+        minTxt.setText(Double.toString(controller.getLowLimit()));
+    }
+
+    public void onUpMaxButton(View view) {
+        controller.onHighLimitUp();
+        updateTextCount();
+    }
+
+    public void onUpMinButton(View view) {
+        controller.onLowLimitUp();
+        updateTextCount();
+    }
+
+    public void onDownMaxButton(View view) {
+        controller.onHighLimitDown();
+        updateTextCount();
+    }
+
+    public void onDownMinButton(View view) {
+        controller.onLowLimitDown();
+        updateTextCount();
     }
 }
--- app/src/main/java/com/w32/tp2_samuel_emma/model/ModelControllerTemperature.java
+++ app/src/main/java/com/w32/tp2_samuel_emma/model/ModelControllerTemperature.java
@@ -1,57 +1,82 @@
 package com.w32.tp2_samuel_emma.model;
 
 import com.w32.tp2_samuel_emma.sensor.SensorData;
+import com.w32.tp2_samuel_emma.sensor.SensorValue;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 //Complétez l'implémentation de la classe avec les méthodes et attributs nécessaires à son fonctionnement
 public class ModelControllerTemperature{
-	
+    SensorData values;
+    SensorValue[] valuesArray;
+    double maxValue;
+    double minValue;
+
 	public ModelControllerTemperature(SensorData p)
     {
-       
+       this.values = p;
+       this.valuesArray = values.getValues();
+       resetMaxAndMinTemperature();
     }
-		
 	//Les méthodes méthodes suivantes sont TOUTES OBLIGATOIRES et leurs signatures DE DOIVENT PAS être modifiées
     public void setHighLimit(double highLimit)
     {
-        
+        this.maxValue = highLimit;
     }
 	
 	public double getHighLimit()
     {
-        // A COMPLETER
-		return 0;
+        return Math.round(maxValue * 100.0) / 100.0;
     }
 
     public void setLowLimit(double lowLimit)
     {
-    
+        this.minValue = lowLimit;
     }	
 
     public double getLowLimit()
     {
-        // A COMPLETER
-		return 0;
+        return Math.round(minValue * 100.0) / 100.0;
     }
 
     public void onHighLimitUp() 
 	{
-    
-
+        maxValue = maxValue + (maxValue - minValue)/10;
     }
 
     public void onHighLimitDown() 
 	{
-    
+	    if(maxValue - (maxValue - minValue)/10 > minValue){
+            maxValue = maxValue - (maxValue - minValue)/10;
+        }
     }
-
 
     public void onLowLimitDown() 
 	{
-    
+	    if(minValue - (maxValue - minValue)/10 >= 0){
+            minValue = minValue - (maxValue - minValue)/10;
+        }
     }
 
     public void onLowLimitUp() 
 	{
-    
-    }  
+	    if(minValue + (maxValue - minValue)/10 < maxValue){
+            minValue = minValue + (maxValue - minValue)/10;
+        }
+    }
+
+    private List<Double> valuesToArrayList(){
+        List<Double> list = new ArrayList<Double>();
+        for (int i = 0; i < valuesArray.length; i++) {
+            list.add(valuesArray[i].getValue());
+        }
+        return list;
+    }
+
+    private void resetMaxAndMinTemperature(){
+	    setHighLimit(Collections.max(valuesToArrayList()));
+	    setLowLimit(Collections.min(valuesToArrayList()));
+    }
 }
