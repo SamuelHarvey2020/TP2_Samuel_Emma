1cb14ad7f31c925daac94f2005db6d46f5223785
Samuel Harvey <shdrawings@outlook.com>
2020-11-27 14:13:03 -0500

    Push Humidity

--- app/src/main/java/com/w32/tp2_samuel_emma/model/ModelControllerHumidity.java
+++ app/src/main/java/com/w32/tp2_samuel_emma/model/ModelControllerHumidity.java
@@ -1,26 +1,32 @@
 package com.w32.tp2_samuel_emma.model;
 
+import com.jjoe64.graphview.series.DataPoint;
 import com.w32.tp2_samuel_emma.sensor.SensorData;
+import com.w32.tp2_samuel_emma.sensor.SensorValue;
 
 //Complétez l'implémentation de la classe avec les méthodes et attributs nécessaires à son fonctionnement
 public class ModelControllerHumidity {
 	public static final double EPSILON = 0.001;
+	private int nbZones = 5;
+	private DataPoint[] dataPointTable ;
 	//Les méthodes méthodes suivantes sont TOUTES OBLIGATOIRES et leurs signatures DE DOIVENT PAS être modifiées
     public ModelControllerHumidity(SensorData p)
     {
 		// A COMPLETER
-    }
+        SensorValue[] sensorDataValues = p.getValues();
 
+    }
 
     public void setNbZones(int nbZones)
     {
-		// A COMPLETER
+        // A COMPLETER
+        this.nbZones = nbZones;
     }
 
     public int getNbZones()
     {
 		// A COMPLETER
-		return 0;
+		return this.nbZones;
     }
 
 
