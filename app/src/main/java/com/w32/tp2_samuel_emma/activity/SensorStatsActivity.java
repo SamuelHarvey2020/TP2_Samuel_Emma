06f0db1f6439e68723e3a9b3ce3174636df49a10
EmFa-1934931 <70659646+EmFa-1934931@users.noreply.github.com>
2020-12-04 15:30:13 -0500

    ajout méthodes SensorDataStatsActivity

--- app/src/main/java/com/w32/tp2_samuel_emma/activity/SensorStatsActivity.java
+++ app/src/main/java/com/w32/tp2_samuel_emma/activity/SensorStatsActivity.java
@@ -3,14 +3,33 @@ package com.w32.tp2_samuel_emma.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.w32.tp2_samuel_emma.R;
+import com.w32.tp2_samuel_emma.database.MyDatabaseFactory;
+import com.w32.tp2_samuel_emma.repository.SensorDataRepository;
 
 public class SensorStatsActivity extends AppCompatActivity {
+
+    private MyDatabaseFactory databaseFactory;
+    private SensorDataRepository repoStat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor_stats);
+
+        databaseFactory = new MyDatabaseFactory(this);
+        repoStat = new SensorDataRepository(databaseFactory.getWritableDatabase());
+    }
+
+    public void onBackButtonPress(View view) {
+        onBackPressed();
+    }
+
+    public void onClickHumidity(View view) {
+    }
+
+    public void onClickTemperature(View view) {
     }
 }
