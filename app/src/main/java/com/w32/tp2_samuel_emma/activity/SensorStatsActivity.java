0c5edf230c327c4dbc74fba0ebdcb20718cba584
EmFa-1934931 <70659646+EmFa-1934931@users.noreply.github.com>
2020-12-04 20:11:08 -0500

    début méthodes dans sensorStatsActivity

--- app/src/main/java/com/w32/tp2_samuel_emma/activity/SensorStatsActivity.java
+++ app/src/main/java/com/w32/tp2_samuel_emma/activity/SensorStatsActivity.java
@@ -6,13 +6,19 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.w32.tp2_samuel_emma.R;
+import com.w32.tp2_samuel_emma.data.SensorDataStats;
 import com.w32.tp2_samuel_emma.database.MyDatabaseFactory;
 import com.w32.tp2_samuel_emma.repository.SensorDataRepository;
+import com.w32.tp2_samuel_emma.sensor.SensorID;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SensorStatsActivity extends AppCompatActivity {
 
     private MyDatabaseFactory databaseFactory;
     private SensorDataRepository repoStat;
+    private List<SensorDataStats> dataList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,9 @@ public class SensorStatsActivity extends AppCompatActivity {
 
         databaseFactory = new MyDatabaseFactory(this);
         repoStat = new SensorDataRepository(databaseFactory.getWritableDatabase());
+
+        //ajout de données dans le spinner
+        populateSpinnerWithData(SensorID.HUMIDITY_ID);
     }
 
     public void onBackButtonPress(View view) {
@@ -28,8 +37,21 @@ public class SensorStatsActivity extends AppCompatActivity {
     }
 
     public void onClickHumidity(View view) {
+        populateSpinnerWithData(SensorID.HUMIDITY_ID);
     }
 
     public void onClickTemperature(View view) {
+        populateSpinnerWithData(SensorID.TEMPERATURE_ID);
+    }
+
+    private void populateSpinnerWithData(SensorID id){
+        List<SensorDataStats> list = new ArrayList<>();
+        //TODO: recherche de données avec findAll à faire
+        this.dataList = list;
+        populateSpinner();
+    }
+
+    private void populateSpinner(){
+
     }
 }
